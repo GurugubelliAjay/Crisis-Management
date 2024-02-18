8ca236ecf30fc7cc4365df5a811ee2bd9135b827
Ajay <124285779+GurugubelliAjay@users.noreply.github.com>
2024-02-18 17:29:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app=express();
 app.engine('ejs',ejsMate);
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({extended:true}));
 const mongoose=require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/volunteer');
@@ -17,6 +18,31 @@ db.once("open",()=>{
     console.log("Database connected");
 })
 
+// Admin Routes
+
+app.get('/admin/dashboard',(req,res)=>{
+    res.render('admin/dashboard')
+})
+app.get('/admin/donors',(req,res)=>{
+    res.render('admin/donors')
+})
+app.get('/admin/resources',(req,res)=>{
+    res.render('admin/resources')
+})
+app.get('/admin/volunteers',(req,res)=>{
+    res.render('admin/volunteers')
+})
+app.get('/admin/complaints',(req,res)=>{
+    res.render('admin/complaints')
+})
+app.get('/admin/alerts',(req,res)=>{
+    res.render('admin/alerts')
+})
+app.get('/admin/news',(req,res)=>{
+    res.render('admin/news')
+})
+
+// Volunteer Routes
 
 app.get('/volunteer/signup',(req,res)=>{
     res.render('volunteers/signup.ejs')
